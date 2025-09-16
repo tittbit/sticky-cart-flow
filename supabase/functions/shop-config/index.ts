460a439e78ed93647c75d34e99eb1922ddd26d05
tittbit <tittbit@gmail.com>
2025-09-16 15:06:55 +0530

    Update index.ts

--- supabase/functions/shop-config/index.ts
+++ supabase/functions/shop-config/index.ts
@@ -3,7 +3,8 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-shop-domain',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-shop-domain, cache-control',
+  'Access-Control-Max-Age': '86400',
 };
 
 const supabase = createClient(
